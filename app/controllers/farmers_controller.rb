295e34919fb502cc8a28d142141dd6ca42a7a84c
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-02-08 19:30:08 +0000

    adds login and authentication to farmers controller

--- app/controllers/farmers_controller.rb
+++ app/controllers/farmers_controller.rb
@@ -3,9 +3,25 @@ class FarmersController < ApplicationController
   get "/login" do 
     erb :login
   end 
+
+  post "/login" do 
+   #params:username, 
+   #find  
+   @farmer = Farmer.find_by(username: params[:username])
+   #authenticate 
+   if @farmer.authenticate(params[:password])
+    session[:farmer_id] = @farmer.id
+    puts session
+    redirect "/farmers/#{@farmer.id}"
+   else 
+   end 
+  end 
   
   get "/signup" do 
     erb :signup 
   end 
   
+  get "/farmers/:id" do 
+    "welcome"
+  end 
 end 
