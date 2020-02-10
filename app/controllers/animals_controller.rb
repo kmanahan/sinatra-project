bf0548def95513b86b6b9ea26945c9b8ddd902f7
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-02-10 20:23:39 +0000

    get by id in animals controller

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -8,14 +8,20 @@ class AnimalsController < ApplicationController
     if logged_in? 
       redirect "/"
     end 
-  if params[:animal] != ""
-  @animal = Animal.create(name: params[:name], species: params[:species], gender: params[:sex], farmer_id: current_user.id)
-    redirect "/animals/#{@animal.id}"
-  else
-    redirect "/animals/new"
+    if params[:animal] != ""
+    @animal = Animal.create(name: params[:name], species: params[:species], gender: params[:sex], farmer_id: current_user.id)
+      redirect "/animals/#{@animal.id}"
+    else
+      redirect "/animals/new"
+    end 
+  end
+
+  get "/animals/:id" do
+  #find by id, dynamic route => look for better definition 
+    @animal = Animal.find_by(id: params[:id])
+    # erb vs redirect = all variable will be 'deleted' if redirected to another route (controller method)
+    erb :"animals/show" 
   end 
-end
-  #find by name 
   #find by species 
   #index of all  
 end 
